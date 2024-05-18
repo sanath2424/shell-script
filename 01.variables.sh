66b376ed1d225a759b04a29c4cf3b3290fc7d9a3
sanath2424 <sanath.mogataram1516@gmail.com>
2024-05-17 17:15:57 -0700

    hello script

--- 01.variables.sh
+++ 01.variables.sh
@@ -1,1 +1,6 @@
-this is a sample to check the code is sync with git 
+# !/bin/bash
+
+echo "Ramu : how are you somu?"
+echo "Somu : i am doing good, how about you?"
+echo "Ramu : Nothing much, upgrading to Devops, and you?"
+echo "Somu : ya i have tried to learn but kind of lost, let me know "
