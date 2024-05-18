2cae8a3c7dc483c703ded88080cc6811a35a004d
sanath2424 <sanath.mogataram1516@gmail.com>
2024-05-17 17:27:10 -0700

    Variables

---
+++ 02-variables.sh
@@ -0,0 +1,9 @@
+# !/bin/bash
+
+$PERSON1=Ramu
+$PERSON2=Somu
+
+echo "$PERSON1 : how are you $PERSON2?"
+echo "$PERSON2 : i am doing good, how about you?"
+echo "$PERSON1 : Nothing much, upgrading to Devops, and you?"
+echo "$PERSON2 : ya i have tried to learn but kind of lost, let me know "
